c2ea56322fcad377aa2cc982c6c4255b9d1e3f75
miara1502 <miara.rakotomavo@gmail.com>
2019-10-26 16:50:20 -0700

    traitement des fichiers

--- deepdrug.py
+++ deepdrug.py
@@ -2,14 +2,13 @@
 
 """Module qui contient tous les fonctions utilisés pour le projet : Deep Learning Binding pocket
 """
-
 import sys
 import os
 import argparse
 
 import numpy as np
-
-#from deepdrug3d import DeepDrug3DBuilder
+import pandas as pd
+import glob
 
 from keras import callbacks
 from keras.optimizers import Adam
@@ -17,22 +16,51 @@ from keras.utils import np_utils
 
 
 #Lecture des datas :
-atps = []
-with open('data/nucleotide.list.txt') as atp_in:
-    for line in atp_in.readlines():
-            temp = line.replace(' ','').replace('\n','')
-            atps.append(temp)
+def load_ligand(file):
+    ''' read a ligand file and return a np data frame
+    '''
+    list_ligand = np.loadtxt(file, dtype = str)
+    return list_ligand
 
-hemes = []
-with open('data/heme.list.txt') as heme_in:
-    for line in heme_in.readlines():
-         temp = line.replace(' ','').replace('\n','')
-         hemes.append(temp)
+def load_voxel(voxel_folder , nucleotide_list , hemes_list,
+    steroid_list, control_list):
+    ''' take a folder , a list of ligand and return a
+    list of targets
+    '''
+    voxel_files = glob.glob(voxel_folder+'\*.npy')
+    #The glob.glob returns the list of files with their
+    #full path (unlike os.listdir())
+    random.shuffle(voxel_files)
+    targets= []
+    features= []
+    controls= []
+    steroids = []
 
-atp_len = len(atps)
-heme_len = len(hemes)
-L = atp_len + heme_len
-voxel = np.zeros(shape = (L, 14, 32, 32, 32),dtype = np.float64)
-label = np.zeros(shape = (L,), dtype = int)
-cnt = 0
+    for voxel in voxel_files[:200]:
+        name = voxel[22:,-4]
+        if name in nucleotide_list:
+            features.append(np.load(voxel))
+            target = [1,0]
+            targets.append(target)
+        elif name in hemes_list:
+            features.append(np.load(voxel))
+            target = [0,1]
+            targets.append(target)
+        elif name in steroid_list:
+            steriod = np.load(voxel)
+            steroides.append(np.reshape(steroid,(14,32,32,32)))
+        elif name in control:
+            c = np.load(voxel)
+            controls.append(np.reshape(c,(14,32,32,32)))
+        else:
+            break
 
+    features = np.array(features)
+    targets = np.array(targets)
+    controls = np.array(controls)
+    steroides = np.array(steroides)
+
+    np.save('features', features)
+    np.save('targets', targets)
+    np.save('controls', controls)
+    np.save('steroides', steroides)
