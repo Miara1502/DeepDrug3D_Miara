530334d5a2085bc676bfd858a5079f40e6a6513b
miara1502 <miara.rakotomavo@gmail.com>
2019-10-28 13:14:04 +0100

    traitement de donnes

--- deepdrug.py
+++ deepdrug.py
@@ -9,10 +9,7 @@ import argparse
 import numpy as np
 import pandas as pd
 import glob
-
-from keras import callbacks
-from keras.optimizers import Adam
-from keras.utils import np_utils
+import random
 
 
 #Lecture des datas :
@@ -22,22 +19,19 @@ def load_ligand(file):
     list_ligand = np.loadtxt(file, dtype = str)
     return list_ligand
 
-def load_voxel(voxel_folder , nucleotide_list , hemes_list,
+def load_voxel(voxel_files , file_number, nucleotide_list , hemes_list,
     steroid_list, control_list):
-    ''' take a folder , a list of ligand and return a
-    list of targets
+    ''' take a folder , a list of ligand and create
+    4 .npY file which correspond of the data that the
+    model is about to use ( features , target ,control
+    and steroid)
     '''
-    voxel_files = glob.glob(voxel_folder+'\*.npy')
-    #The glob.glob returns the list of files with their
-    #full path (unlike os.listdir())
-    random.shuffle(voxel_files)
     targets= []
     features= []
     controls= []
     steroids = []
-
-    for voxel in voxel_files[:200]:
-        name = voxel[22:,-4]
+    for voxel in voxel_files[:file_number]:
+        name = voxel[22:-4]
         if name in nucleotide_list:
             features.append(np.load(voxel))
             target = [1,0]
@@ -47,20 +41,41 @@ def load_voxel(voxel_folder , nucleotide_list , hemes_list,
             target = [0,1]
             targets.append(target)
         elif name in steroid_list:
-            steriod = np.load(voxel)
-            steroides.append(np.reshape(steroid,(14,32,32,32)))
-        elif name in control:
+            steroid = np.load(voxel)
+            steroids.append(np.reshape(steroid,(14,32,32,32)))
+        elif name in control_list:
             c = np.load(voxel)
             controls.append(np.reshape(c,(14,32,32,32)))
         else:
             break
-
     features = np.array(features)
     targets = np.array(targets)
+    print(targets)
     controls = np.array(controls)
-    steroides = np.array(steroides)
-
+    steroids = np.array(steroids)
+    '''
     np.save('features', features)
     np.save('targets', targets)
     np.save('controls', controls)
-    np.save('steroides', steroides)
+    np.save('steroides', steroids)
+    '''
+def main():
+    parser=argparse.ArgumentParser(description="Deepdrug traitement fichiers.")
+    parser.add_argument("-n", "--nb-files",
+                        help="Selectionner le nombre de fichiers voxel à utiliser",
+                        type=int, default=200, dest="nFile")
+    args = parser.parse_args()
+
+    voxel_folder = 'deepdrug3d_voxel_data'
+    files = glob.glob(voxel_folder+'/*.npy')
+    random.shuffle(files)
+    nucleotides_list = load_ligand('nucleotide.list')
+    hemes_list = load_ligand('heme.list')
+    steroid_list = load_ligand('steroid.list')
+    control_list = load_ligand('control.list')
+
+    load_voxel(files, args.nFile, nucleotides_list , hemes_list , steroid_list,
+    control_list)
+
+if __name__ == "__main__":
+    main()
