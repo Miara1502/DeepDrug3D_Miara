1ef58a1e6bed8daef3589beb6a16f848d46180cb
miara1502 <miara.rakotomavo@gmail.com>
2019-10-28 16:12:07 +0100

    Comparaison des 2 modeles

--- new_model.py
+++ new_model.py
@@ -18,31 +18,27 @@ model = Sequential()
 
         # 1ère convolution
 
-model.add(Convolution3D(input_shape = (14,32,32,32), filters=64, kernel_size=8, padding='valid', data_format='channels_first'))
+model.add(Convolution3D(input_shape = (14,32,32,32), filters=64, kernel_size=5, padding='valid', data_format='channels_first'))
 model.add(LeakyReLU(alpha = 0.1))
-model.add(MaxPooling3D(pool_size=(2,2,2), padding='valid', data_format='channels_first'))
+        # Dropout 1
 model.add(Dropout(0.2))
 
         # 2ème convolution
 
-model.add(Convolution3D(filters=32, kernel_size=5, padding='valid', data_format='channels_first',))
-model.add(LeakyReLU(alpha = 0.2))
-model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid', data_format='channels_first'))
-model.add(Dropout(0.4))
-
-        # 3ème convolution
-#kernel_size = 5
-model.add(Convolution3D(filters=128, kernel_size=3, padding='valid', data_format='channels_first',))
+model.add(Convolution3D(filters=32, kernel_size=3, padding='valid', data_format='channels_first',))
 model.add(LeakyReLU(alpha = 0.1))
 model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid', data_format='channels_first'))
+        # Dropout 2
 model.add(Dropout(0.4))
 
         # FC 1
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(128)) # TODO changed to 64 for the CAM
 model.add(LeakyReLU(alpha = 0.1))
+
+        # Dropout 3
 model.add(Dropout(0.4))
-        # Fully connected layer 2 to shape (2) for 2 classes
+# Fully connected layer 2 to shape (2) for 2 classes
 model.add(Dense(2))
 model.add(Activation('softmax'))
 
@@ -52,10 +48,3 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 model.fit(X_train, y_train, validation_data=(X_valid, y_valid), epochs=3)
 
 model.summary()
-
-## Prédiction
-'''
-Y_control = model.predict(X_control[:4])
-for i in range(4):
-    print("X=%s, Predicted=%s" % (X_control[i], Y_control[i]))
-'''
