146cb15ec400543890c5e68bb0acd604c44b3fc3
miara1502 <miara.rakotomavo@gmail.com>
2019-10-29 15:22:20 +0100

    model a 70 accuracy

--- model_deep.py
+++ model_deep.py
@@ -18,14 +18,14 @@ from sklearn.model_selection import train_test_split
 def my_model():
     model = Sequential()
             # 1ere Convolution
-    model.add(Convolution3D(input_shape = (14,32,32,32), filters=64, kernel_size=5, padding='valid', data_format='channels_first'))
+    model.add(Convolution3D(input_shape = (14,32,32,32), filters=64, kernel_size=8, padding='valid', data_format='channels_first'))
     model.add(LeakyReLU(alpha = 0.1))
     #model.add(MaxPooling3D(pool_size=(2,2,2), padding='valid', data_format='channels_first'))
             # Dropout 1
     model.add(Dropout(0.2))
 
             # 2ème convolution
-    model.add(Convolution3D(filters=64, kernel_size=3, padding='valid', data_format='channels_first',))
+    model.add(Convolution3D(filters=64, kernel_size=5, padding='valid', data_format='channels_first',))
     model.add(LeakyReLU(alpha = 0.2))
     model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid', data_format='channels_first'))
             # Dropout 2
@@ -67,28 +67,22 @@ def main():
     print(features.shape)
     print(targets)
     y = np_utils.to_categorical(targets , num_classes = 3)
-    print('hello ici normalement')
-    print(y)
-
-
     x_train,x_test,y_train,y_test=train_test_split(
     features, y,test_size=0.2,random_state=42)
-
-
     print(x_train.shape)
     print(x_test.shape)
     print(y_train.shape)
     print(y_test.shape)
-
+    #Création du model
     model = my_model()
     print(model.input_shape)
     print(model.output_shape)
-
     model.summary()
     model_training = training(model,x_train , y_train , x_test , y_test)
+    #Prédiction
 
-
-
-
+    y_pred = prediction(model, x_test)
+    print(y_pred)
+    
 if __name__ == "__main__":
     main()
