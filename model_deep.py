742d0f43997655a60a7c5ccd0fb691c310db3696
miara1502 <miara.rakotomavo@gmail.com>
2019-10-29 04:54:12 +0100

    model pas encore fonctionnel et hasardeux

--- model_deep.py
+++ model_deep.py
@@ -18,45 +18,37 @@ from sklearn.model_selection import train_test_split
 def my_model():
     model = Sequential()
             # 1ere Convolution
-    model.add(Convolution3D(input_shape=(14,32,32,32),filters=64,kernel_size=5,
-                            padding='valid', data_format='channels_first'))
+    model.add(Convolution3D(input_shape = (14,32,32,32), filters=64, kernel_size=5, padding='valid', data_format='channels_first'))
     model.add(LeakyReLU(alpha = 0.1))
+    model.add(MaxPooling3D(pool_size=(2,2,2), padding='valid', data_format='channels_first'))
             # Dropout 1
-    model.add(Dropout(0.2))
+    #model.add(Dropout(0.2))
+
             # 2ème convolution
-    model.add(Convolution3D(filters=64, kernel_size=3, padding='valid',
-                            data_format='channels_first',))
+    model.add(Convolution3D(filters=128, kernel_size=3, padding='valid', data_format='channels_first',))
     model.add(LeakyReLU(alpha = 0.1))
-    model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid',
-                            data_format='channels_first'))
+    model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid', data_format='channels_first'))
             # Dropout 2
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.2))
 
-
-            # 3em Convolution
-    model.add(Convolution3D(filters=128, kernel_size=2, padding='valid',
-                            data_format='channels_first',))
-    model.add(LeakyReLU(alpha = 0.1))
-    model.add(MaxPooling3D(pool_size=(2,2,2), strides=None, padding='valid',
-                            data_format='channels_first'))
-
-            # Fully connected layer 1
+            # FC 1
     model.add(Flatten())
     model.add(Dense(128))
     model.add(LeakyReLU(alpha = 0.1))
-
-    # Dropout 3
+            # Dropout 3
     model.add(Dropout(0.4))
-
+    # Fully connected layer 2 to shape (2) for 2 classes
     model.add(Dense(3))
     model.add(Activation('softmax'))
+
+
     return model
 
 def training(model , x_train , y_train , x_test , y_test):
     model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
     EarlyStopping(monitor='val_loss',min_delta=0.1,patience=6,verbose=2,mode='auto',
     baseline=None, restore_best_weights=False)
-    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1, batch_size = 50)
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=3, batch_size = 10)
     val_loss , val_acc = model.evaluate(x_test, y_test)
     print(model.metrics_names)
     print(val_loss , val_acc)
